1dc4a2ea3e27ee9b71021112afc84aac37daed4e
Jonathan Olson <olsonsjc@gmail.com>
2017-02-27 12:30:36 -0700

    Fix Easing reference

--- js/demo/EasingDemoView.js
+++ js/demo/EasingDemoView.js
@@ -130,9 +130,9 @@ define( function( require ) {
       { node: new Text( 'Quartic in-out', comboTextOptions ), value: Easing.QUARTIC_IN_OUT },
       { node: new Text( 'Quartic in', comboTextOptions ), value: Easing.QUARTIC_IN },
       { node: new Text( 'Quartic out', comboTextOptions ), value: Easing.QUARTIC_OUT },
-      { node: new Text( 'Qunitic in-out', comboTextOptions ), value: Easing.QUNTIC_IN_OUT },
-      { node: new Text( 'Qunitic in', comboTextOptions ), value: Easing.QUNTIC_IN },
-      { node: new Text( 'Qunitic out', comboTextOptions ), value: Easing.QUNTIC_OUT },
+      { node: new Text( 'Qunitic in-out', comboTextOptions ), value: Easing.QUINTIC_IN_OUT },
+      { node: new Text( 'Qunitic in', comboTextOptions ), value: Easing.QUINTIC_IN },
+      { node: new Text( 'Qunitic out', comboTextOptions ), value: Easing.QUINTIC_OUT },
     ], easingProperty, listParent, {
       right: this.layoutBounds.right - 10,
       top: 10

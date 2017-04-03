7359a69cb88f8cb7d15eb3d8c1ea61e8f54150e0
Chris Malley <cmalley@pixelzoom.com>
2017-04-03 11:42:45 -0600

    fix copyright dates

--- js/EaseAnimation.js
+++ js/EaseAnimation.js
@@ -1,4 +1,4 @@
-// Copyright 2016, University of Colorado Boulder
+// Copyright 2017, University of Colorado Boulder
 
 /**
  * WARNING: PROTOTYPE, see https://github.com/phetsims/twixt/issues/3 before using!
--- js/Easing.js
+++ js/Easing.js
@@ -1,4 +1,4 @@
-// Copyright 2016, University of Colorado Boulder
+// Copyright 2017, University of Colorado Boulder
 
 /**
  * An Easing represents a function from the range [0,1] => [0,1] where f(0)=0 and f(1)=1. It is helpful for animation,

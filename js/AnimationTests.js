f9c24bedd318f6d219b1473c5950b75321c695fd
samreid <reids@colorado.edu>
2018-04-24 16:52:31 -0600

    Added simple unit test, see https://github.com/phetsims/twixt/issues/11

---
+++ js/AnimationTests.js
@@ -0,0 +1,41 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ * Animation tests
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var Animation = require( 'Animation' );
+  var NumberProperty = require( 'AXON/NumberProperty' );
+
+  QUnit.module( 'Animation' );
+
+  QUnit.test( 'basic animation tests', function( assert ) {
+    assert.equal( 1, 1, 'sanity check' );
+
+    var numberProperty = new NumberProperty( 0 );
+
+    var targetValue = 7;
+    var animation = new Animation( {
+      // Options for the Animation as a whole
+      duration: 2,
+
+      // Options for the one target to change
+      property: numberProperty,
+      to: targetValue
+    } );
+    animation.start();
+    for ( var i = 0; i < 10; i++ ) {
+      animation.step( 0.1 );
+    }
+    assert.ok( Math.abs( numberProperty.value - targetValue / 2 ) < 1E-6, 'should be halfway there' );
+    for ( i = 0; i < 10; i++ ) {
+      animation.step( 0.1 );
+    }
+    assert.ok( Math.abs( numberProperty.value - targetValue ) < 1E-6, 'should be all the way there' );
+  } );
+} );

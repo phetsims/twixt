d3846823ace9594e5cf12997f6c3a8a67476e49f
zepumph <michael.kauzmann@colorado.edu>
2019-10-22 13:39:34 -0800

    add UTTERANCE_QUEUE to configs, move tests over to utterance-queue repo, convert usages to use the library, not the scenery-phet ones, delete those, https://github.com/phetsims/utterance-queue/issues/2

--- js/twixt-config.js
+++ js/twixt-config.js
@@ -40,7 +40,8 @@ require.config( {
     SUN: '../../sun/js',
     TAMBO: '../../tambo/js',
     TANDEM: '../../tandem/js',
-    TWIXT: '.'
+    TWIXT: '.',
+    UTTERANCE_QUEUE: '../../utterance-queue/js'
   },
 
   // Cache busting is applied by default, but can be disabled via ?cacheBust=false, see initialize-globals.js

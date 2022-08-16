26a81e31a45b83af98ecf7a586a48357eaba869f
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:43 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/twixtStrings.ts
+++ js/twixtStrings.ts
@@ -5,11 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import twixt from './twixt.js';
 
 type StringsType = {
   'twixt': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   }
 };
 

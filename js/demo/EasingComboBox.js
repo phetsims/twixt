1f35284c06a7bba3ab04eae44429d3f81df8144f
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-01-13 16:17:43 -0700

    remove unused tandem parameter from createNode, https://github.com/phetsims/sun/issues/821

--- js/demo/EasingComboBox.js
+++ js/demo/EasingComboBox.js
@@ -23,19 +23,19 @@ class EasingComboBox extends ComboBox {
 
     const comboTextOptions = { font: new PhetFont( 16 ) };
     const items = [
-      { value: Easing.LINEAR, createNode: tandem => new Text( 'Linear', comboTextOptions ) },
-      { value: Easing.QUADRATIC_IN_OUT, createNode: tandem => new Text( 'Quadratic in-out', comboTextOptions ) },
-      { value: Easing.QUADRATIC_IN, createNode: tandem => new Text( 'Quadratic in', comboTextOptions ) },
-      { value: Easing.QUADRATIC_OUT, createNode: tandem => new Text( 'Quadratic out', comboTextOptions ) },
-      { value: Easing.CUBIC_IN_OUT, createNode: tandem => new Text( 'Cubic in-out', comboTextOptions ) },
-      { value: Easing.CUBIC_IN, createNode: tandem => new Text( 'Cubic in', comboTextOptions ) },
-      { value: Easing.CUBIC_OUT, createNode: tandem => new Text( 'Cubic out', comboTextOptions ) },
-      { value: Easing.QUARTIC_IN_OUT, createNode: tandem => new Text( 'Quartic in-out', comboTextOptions ) },
-      { value: Easing.QUARTIC_IN, createNode: tandem => new Text( 'Quartic in', comboTextOptions ) },
-      { value: Easing.QUARTIC_OUT, createNode: tandem => new Text( 'Quartic out', comboTextOptions ) },
-      { value: Easing.QUINTIC_IN_OUT, createNode: tandem => new Text( 'Quintic in-out', comboTextOptions ) },
-      { value: Easing.QUINTIC_IN, createNode: tandem => new Text( 'Quintic in', comboTextOptions ) },
-      { value: Easing.QUINTIC_OUT, createNode: tandem => new Text( 'Quintic out', comboTextOptions ) }
+      { value: Easing.LINEAR, createNode: () => new Text( 'Linear', comboTextOptions ) },
+      { value: Easing.QUADRATIC_IN_OUT, createNode: () => new Text( 'Quadratic in-out', comboTextOptions ) },
+      { value: Easing.QUADRATIC_IN, createNode: () => new Text( 'Quadratic in', comboTextOptions ) },
+      { value: Easing.QUADRATIC_OUT, createNode: () => new Text( 'Quadratic out', comboTextOptions ) },
+      { value: Easing.CUBIC_IN_OUT, createNode: () => new Text( 'Cubic in-out', comboTextOptions ) },
+      { value: Easing.CUBIC_IN, createNode: () => new Text( 'Cubic in', comboTextOptions ) },
+      { value: Easing.CUBIC_OUT, createNode: () => new Text( 'Cubic out', comboTextOptions ) },
+      { value: Easing.QUARTIC_IN_OUT, createNode: () => new Text( 'Quartic in-out', comboTextOptions ) },
+      { value: Easing.QUARTIC_IN, createNode: () => new Text( 'Quartic in', comboTextOptions ) },
+      { value: Easing.QUARTIC_OUT, createNode: () => new Text( 'Quartic out', comboTextOptions ) },
+      { value: Easing.QUINTIC_IN_OUT, createNode: () => new Text( 'Quintic in-out', comboTextOptions ) },
+      { value: Easing.QUINTIC_IN, createNode: () => new Text( 'Quintic in', comboTextOptions ) },
+      { value: Easing.QUINTIC_OUT, createNode: () => new Text( 'Quintic out', comboTextOptions ) }
     ];
 
     super( easingProperty, items, listParent, options );

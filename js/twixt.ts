4e09b5daf7c400aafa03008f1f84f2b000ab6807
pixelzoom <cmalley@pixelzoom.com>
2024-01-05 09:22:41 -0700

    fixes for author-annotation lint rule, https://github.com/phetsims/chipper/issues/1414

--- js/twixt.ts
+++ js/twixt.ts
@@ -1,5 +1,8 @@
 // Copyright 2016-2023, University of Colorado Boulder
 
+/**
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
 
 import Namespace from '../../phet-core/js/Namespace.js';
 

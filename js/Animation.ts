0f3b8ac8e973c12bd9b9ce30e1e583bb1bf4cd84
Jonathan Olson <jonathan.olson@colorado.edu>
2024-02-07 10:18:20 -0700

    Revert "rename ConstantStepEmitter to ConstantDtClock, https://github.com/phetsims/faradays-electromagnetic-lab/issues/69"
    See https://github.com/phetsims/phet-lib/issues/2
    
    This reverts commit b61079e7f49b6516c2c8b0726e19d7db523d156c.

--- js/Animation.ts
+++ js/Animation.ts
@@ -190,7 +190,7 @@ class Animation<SelfType = unknown, SelfObjectType = unknown, TargetTypes = unkn
       'The delay should be a non-negative number.' );
 
     assert && assert( config.stepEmitter === null || config.stepEmitter instanceof Emitter || config.stepEmitter instanceof TinyEmitter,
-      'clock must be null or an (Tiny)Emitter' );
+      'stepEmitter must be null or an (Tiny)Emitter' );
 
     super( config );
 

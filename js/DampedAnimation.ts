3857dec6a991b173530d547de0e6cda53c2a6310
phet-dev <phetadmin@gmail.com>
2025-09-16 04:20:58 -0600

    update copyright dates from daily grunt work

--- js/DampedAnimation.ts
+++ js/DampedAnimation.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2023, University of Colorado Boulder
+// Copyright 2017-2025, University of Colorado Boulder
 
 /**
  * WARNING: PROTOTYPE, see https://github.com/phetsims/twixt/issues/3 before using!
